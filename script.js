2cf96b14824d5b16291550b1787d4267353ec9df
IrfanRaza9 <98387965+IrfanRaza9@users.noreply.github.com>
2024-01-04 17:10:57 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+var index=0;
+
+function changeColors(){
+    let colors = ['red','green','skyblue','black','orange','yellow'];
+    document.getElementsByTagName('body')[0].style.background=colors[index++];
+
+    if(index > colors.length-1){
+        index=0;
+    }
+}
